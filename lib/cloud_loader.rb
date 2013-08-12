96e091b3417c282a5b1bce8a4abfdc9e7a50219f
Charles H Martin <charlesmartin14@gmail.com>
2013-08-11 19:30:30 -0700

    .

--- lib/cloud_loader.rb
+++ lib/cloud_loader.rb
@@ -1,3 +1,10 @@
+#
+# Copyright (c) 2013 Charles H Martin, PhD
+#  
+#  Calculated Content 
+#  http://calculatedcontent.com
+#  charles@calculatedcontent.com
+#
 require 'cloud_loader'
 require 'cloud_loader/version'
 require 'cloud_loader/cloud_loader'
--- lib/cloud_loader/version.rb
+++ lib/cloud_loader/version.rb
@@ -1,3 +1,10 @@
+#
+# Copyright (c) 2013 Charles H Martin, PhD
+#  
+#  Calculated Content 
+#  http://calculatedcontent.com
+#  charles@calculatedcontent.com
+#
 module RedisCaches
   VERSION = "0.1"
 end
